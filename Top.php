aa100e4632cc716ea1fd646c004f6a3d40aaf855
VitaForas <122243567+Vitaforas@users.noreply.github.com>
2026-02-09 21:29:42 +0100

    Update Top.php

--- Top.php
+++ Top.php
@@ -1,1 +1,147 @@
+<!DOCTYPE html>
+<html>
+<html lang="en">
 
+
+<head>
+<meta charset="utf-8">
+
+
+<meta name="viewport" content="width=750">
+
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+
+<meta name="description" content="vitaforas top 3 ebay shop java! Lighter | Official // buntes Feuerzeugbenzin farbig Petroleum retro Farbe färben lighter fuel farbiges Benzin Feuerzeug Kraftstoff cyber Vintage best buy online bestellen order" />
+
+
+
+
+<title>vitaforas | Official</title>
+
+
+
+<body style="background-image: url('background.jpg'); background-size: cover; text-align: center;">
+
+
+<link rel="image_src" href="background.jpg" />
+<meta property="og:image" content="background.jpg" />
+<meta property="og:description" content="java! Lighter | Official" />
+<meta property="og:site_name" content="Official"/>
+<meta property="og:title" content="Official"/>
+<meta property="og:url" content="https://buy"/>
+
+
+    <style>
+        body {
+            background-image: url('background.jpg');
+            background-size: cover;
+            text-align: center;
+        }
+    </style>
+</head>
+_
+
+<body> 
+<div>
+
+
+<p></p>
+    </div>
+
+ 
+
+    <h1>   
+<img src="1.gif" alt="Logo">
+ </h1>
+
+
+
+
+
+
+
+
+<p>.</p>
+
+<p>.</p>
+
+<p>       	<a href="https://www.ebay.de/usr/vitaforas" target="_blank" target="_blank" ><img src="klick.png" alt="klick/buy.html" width="75%" height="auto" class="link"></a></p>
+
+
+<p>.</p>
+<p>.</p>
+
+<p> 	_ _ _ _ _ ________________________ ______________ <a href="Feuer.html" ><img src="Feuer.png" alt="⭐Buy.html" width="25%" height="auto" class="link"></a> </p>
+
+
+
+
+
+
+<p>.</p>
+
+<p> ______________ ______________ _________ ________________ 	<a href="https://youtube.com/@vitaforas_de?sub_confirmation=1" target="_blank" ><img src="8.png" alt="✅Seite1.html" width="25%" height="auto" class="link"></a> </p>
+
+<p>.</p>
+
+<p> __________________________ ____________	<a href="http://laser.vitaforas.de" target="_blank" ><img src="Picsart_24-10-02_02-59-11-391.png" alt="📺" width="25%" height="auto" class="link"></a> </p>
+
+
+<p>.</p>
+
+
+
+<p>.</p>
+
+
+
+<p>.</p>
+
+
+<p>.</p>
+
+
+<p> 	<a href="http://home.vitaforas.de" target="_blank" ><img src="Picsart_25-04-21_09-01-05-982.png" alt="🔥Feuer.html" width="50%" height="auto" class="link"></a> </p>
+
+
+
+
+
+
+
+<p>.</p>
+
+<p>.</p>
+
+<p>.</p>
+
+
+<p>.</p>
+
+<p>.</p>
+
+
+
+     <a href="https://youtube.com/@vitaforas_de?sub_confirmation=1" target="_blank" class="social"><img src="img/insta.png" alt="Instagram" width="24" height="28"></a>
+                <a href="https://youtube.com/@vitaforas_de?sub_confirmation=1" target="_blank" class="social"><img src="img/fb.png" alt="Facebook" width="22" height="28"></a>
+                <a href="https://youtube.com/@vitaforas_de?sub_confirmation=1" target="_blank" class="social"><img src="img/spotify.png" alt="Spotify" width="28" height="28"></a>
+                <a href="https://youtube.com/@vitaforas_de?sub_confirmation=1" target="_blank" class="social"><img src="img/twitter.png" alt="Twitter" width="31" height="28"></a>
+                <a href="https://youtube.com/@vitaforas_de?sub_confirmation=1" target="_blank" class="social"><img src="img/soundcloud.png" alt="Soundcloud" width="48" height="28"></a>
+                <a href="https://youtube.com/@vitaforas_de?sub_confirmation=1" target="_blank" class="social"><img src="img/youtube.png" alt="Youtube" width="35" height="28"></a>
+                <br>
+
+                <div class="blue">
+
+                <a href="https://youtube.com/@vitaforas_de?sub_confirmation=1" target="_blank" ><img src="img/merchandise.png" alt="Watch" width="131" height="20" class="link"></a>
+
+          <a href="https://youtube.com/@vitaforas_de?sub_confirmation=1" target="_blank" ><img src="img/schedule.png" alt="Free" width="94" height="20" class="link"></a>
+
+                <a href="mailto:kontakt@vitaforas.de"><img src="img/booking.png" alt="@" width="92" height="20" class="link"></a>
+                        <a href="https://youtube.com/@vitaforas_de?sub_confirmation=1" target="_blank"><img src="img/vote.png" alt="StreamTV" width="45" height="20" class="link"></a>
+        </div>
+                </div>
+</div>
+
+</body>
+</html>
